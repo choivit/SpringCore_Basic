172892e26a3a06913b12ea31f3eabc68522c3ab7
choivit <topvit5@hanmail.net>
2022-10-26 09:16:33 +0900

    221026 AppConfig 리팩터링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -11,11 +13,36 @@ public class AppConfig {
 
     //AppConfig는 공연기획자와 같은 역할. 구체적인 건 여기서 선택을 해주기. 예)배우캐스팅
     //너무 많은 책임을 지고 있었던 Impl클래스의 역할과 책임을 적절하게 나누어 단일 책임 원칙을 지킬 수 있음.
+//    public MemberService memberService(){
+//        return new MemberServiceImpl(new MemoryMemberRepository());
+//    }
+//
+//    public OrderService orderService(){
+//        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+//    }
+
+//  ========================================================================================================
+
+    //하지만 역할과 구현은 또 정확하게 나뉘어야 하기 때문에
+    //MemoryMemberRepository()라는 보이지 않기 때문에
+
+    //memberService 역할
     public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
-    public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    //memberRepository 역할
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
+
+    //memberRepository 역할
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
+    }
+
 }
