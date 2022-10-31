c18dbde37a36a4d92d42fb355a02f0e7b1f2f560
choivit <topvit5@hanmail.net>
2022-10-31 10:16:05 +0900

    221031 다양한 설정 형식 지원 - 자바 코드, XML

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,22 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext(){
+        ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        //isInstanceOf는 해당 연상자가 괄호안에 연산자가 맞는지 확인하는 부분
+        assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}
