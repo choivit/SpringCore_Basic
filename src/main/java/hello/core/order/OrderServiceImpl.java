6d922322ee0b0e5a7a99f9f8ca51a22649b6d292
choivit <topvit5@hanmail.net>
2022-11-02 16:48:35 +0900

    221102 다양한 의존관계 주입 방법

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,19 +11,71 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService{
 
+//    ====================================================================================
+    /*생성자 주입*/
     //    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final MemberRepository memberRepository;
+//    private final MemberRepository memberRepository;
 
     //변경 전
 //    private DiscountPolicy discountPolicy = new RateDiscountPolicy();
 //    private DiscountPolicy discountPolicy = new FixDiscountPolicy();
 
     //인터페이스에만 의존하도록 변경 후(DIP를 지켰다.)
-    private final DiscountPolicy discountPolicy;
+//    private final DiscountPolicy discountPolicy;
 
+//    ====================================================================================
+    /*수정자 주입*/
+//    private MemberRepository memberRepository;
+//    private DiscountPolicy discountPolicy;
+//
+//    //수정자
+//    @Autowired(required = false)
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        System.out.println("setDiscountPolicy = " + discountPolicy);
+//        this.discountPolicy = discountPolicy;
+//    }
+//
+//    //수정자
+//    @Autowired
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        System.out.println("setMemberRepository = " + memberRepository);
+//        this.memberRepository = memberRepository;
+//    }
+
+//    ====================================================================================
+    /*자바빈 프로퍼티*/
+
+//    ====================================================================================
+    /*필드 주입*/
+    //외부에서 확인이 불가능해서 좋은 방법이 아님.
+
+//    @Autowired private MemberRepository memberRepository;
+//    @Autowired private DiscountPolicy discountPolicy;
+
+    //    ====================================================================================
+    /*일반 메서드 주입*/
+
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
     @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+//    ====================================================================================
+    //생성자
+//    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
         this.discountPolicy = discountPolicy;
     }
 
--- src/test/java/hello/core/CoreApplicationTests.java
+++ src/test/java/hello/core/CoreApplicationTests.java
@@ -1,10 +1,16 @@
 package hello.core;
 
+import hello.core.order.OrderService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
+
+	//테스트 코드는 나만 테스트하는데 쓰면 돼서 쓰는 경우도 종종!
+//	@Autowired
+//	OrderService orderService;
 
 	@Test
 	void contextLoads() {
