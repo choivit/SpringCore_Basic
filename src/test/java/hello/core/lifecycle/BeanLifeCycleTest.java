699df5439428e16d5d97b4c2d341de00abdbfe12
choivit <topvit5@hanmail.net>
2022-11-05 15:44:17 +0900

    221105 빈 생명주기 콜백 시작

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +11,12 @@ public class BeanLifeCycleTest {
 
     @Test
     public void lifeCycleTest(){
-
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+        //이전과는 다르게 close를 적용해볼건데
+        // 상단에 AnnotationConfigApplicationContext의 정의방법으로 ApplicationContext는 close를 사용할 수 없음.
+        // 따라서 AnnotationConfigApplicationContext 혹은 ConfigurableApplicationContext로 적용을 해줘야 사용 가능함.
     }
 
     @Configuration
@@ -16,6 +24,8 @@ public class BeanLifeCycleTest {
         @Bean
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
         }
     }
 }
