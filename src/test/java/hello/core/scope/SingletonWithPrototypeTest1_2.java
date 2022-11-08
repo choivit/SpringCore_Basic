79796fc0ca7c3230b893ce8b7fcfc63bb1a35852
choivit <topvit5@hanmail.net>
2022-11-08 14:28:41 +0900

    221108 :: 프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1_2.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1_2.java
@@ -1,18 +1,19 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SingletonWithPrototypeTest1 {
+public class SingletonWithPrototypeTest1_2 {
 
     //스프링 컨테이너에 프로토타입 빈 직접 요청
     @Test
@@ -36,21 +37,23 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     //default가 싱글톤이라 안해줘도 되지만
     @Scope("singleton")
     static class ClientBean{
-        private  final PrototypeBean prototypeBean; //생성시점에 주입 x@1
 
-        //생성자가 하나라 굳이 @Autowired 필요없음.
         @Autowired
-        ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+//        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//        private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+        private Provider<PrototypeBean> prototypeBeanProvider;
+        //ObjectFactory가 과거에 있고 편의기능을 추가하여 ObjectProvider가 만들어짐.
+        // prototypeBeanProvider.getObject를 호출하면 그때 스프링 컨테이너에서 프로토타입 빈을 찾아서 호출해줌.
 
         public int logic(){
+//            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
@@ -98,4 +101,6 @@ public class SingletonWithPrototypeTest1 {
             System.out.println("PrototypeBean.destroy");
         }
     }
+
+    //
 }
